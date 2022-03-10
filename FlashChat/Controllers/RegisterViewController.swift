4f333b79126979dd2e24326d1f9f0d579466d5af
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-03-10 13:41:59 +0200

    added registering functionality

--- FlashChat/Controllers/RegisterViewController.swift
+++ FlashChat/Controllers/RegisterViewController.swift
@@ -6,16 +6,29 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    @IBAction func registerPressed(_ sender: UIButton) {
+
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+
+            }
+
+        }
     }
-    
+
 }
