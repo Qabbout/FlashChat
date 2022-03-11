54f10589d51b6b0b1aae99031f85a624228b042b
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-03-11 23:40:31 +0200

    added loading messages on login functionality

--- FlashChat/Controllers/ChatViewController.swift
+++ FlashChat/Controllers/ChatViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import Firebase
+
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
 
     let db = Firestore.firestore()
+
+    var messages: [Message] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,12 +26,46 @@ class ChatViewController: UIViewController {
 
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
 
+        loadMessages()
+
+    }
+
+    func loadMessages() {
+
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+
+            if let error = error {
+                print(error)
+            }
+                
+            else {
+                self.messages = []
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let data = doc.data()
+                        if let messageSender = data[K.FStore.senderField] as? String,
+                            let messageBody = data[K.FStore.bodyField] as? String {
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            self.messages.append(newMessage)
+
+                            DispatchQueue.main.async { [weak self] in
+                                self?.tableView.reloadData()
+                            }
+
+                        }
+                    }
+                }
+            }
+
+        }
     }
 
     @IBAction func sendPressed(_ sender: UIButton) {
 
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { error in
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970]) { error in
                 if let error = error {
                     print("Couldnt save data to Firestore \(error)")
                 }
@@ -53,12 +90,12 @@ class ChatViewController: UIViewController {
 extension ChatViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
-        cell.label.text = "text"
+        cell.label.text = messages[indexPath.row].body
         return cell
 
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        3
+        messages.count
     }
 
 
